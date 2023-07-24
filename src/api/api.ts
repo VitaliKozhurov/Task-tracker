fe5a096c2c57f2d0c975d4b807c41d45a4a6dd2b
VitaliKozhurov <vitalik24021992@gmail.com>
2023-07-24 12:31:25 +0300

    Api for request to server

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,9 @@
-export type TodoListServerType = {
-    id: string;
-    title: string;
-    addedDate: string;
-    order: number;
-};
+import axios, { AxiosResponse } from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    withCredentials: true,
+});
 
 export enum TaskStatuses {
     New = 0,
@@ -20,6 +20,92 @@ export enum TaskPriorities {
     Later = 4,
 }
 
+export class AuthAPI {
+    static authMe() {
+        return instance.get<ResponseType<AuthResponseType>>('auth/me');
+    }
+
+    static login(data: LoginRequestType) {
+        return instance.post<
+            ResponseType<LoginResponseType>,
+            AxiosResponse<ResponseType<LoginResponseType>>,
+            LoginRequestType
+        >('auth/login', data);
+    }
+
+    static logout() {
+        return instance.delete<ResponseType>('auth/login');
+    }
+}
+
+export class TodoListAPI {
+    static getTodoLists() {
+        return instance.get<TodoListServerType>('todo-lists');
+    }
+
+    static createTodoList(data: { title: string }) {
+        return instance.post<
+            ResponseType<CreateTodoListType>,
+            AxiosResponse<ResponseType<AxiosResponse>>,
+            { title: string }
+        >('todo-lists', data);
+    }
+
+    static updateTodoList(todoListID: string, title: { title: string }) {
+        return instance.put<ResponseType, AxiosResponse<ResponseType>, { title: string }>(
+            `todo-lists/${todoListID}`,
+            title,
+        );
+    }
+
+    static deleteTodoList(todoListID: string) {
+        return instance.delete(`todo-lists/${todoListID}`);
+    }
+
+    static reorderTodoList(todoListID: string, data: { putAfterItemId: string }) {
+        return instance.put<ResponseType, AxiosResponse<ResponseType>, { putAfterItemId: string }>(
+            `todo-lists/${todoListID}/reorder`,
+            data,
+        );
+    }
+}
+
+export class TasksAPI {
+    static getTasks(todoListID: string) {
+        return instance.get<GetTasksResponseType>(`todo-lists/${todoListID}/tasks`);
+    }
+
+    static createTask(todoListID: string, title: { title: string }) {
+        return instance.post<
+            ResponseType<TaskResponseType>,
+            AxiosResponse<ResponseType<TaskResponseType>>,
+            { title: string }
+        >(`todo-lists/${todoListID}/tasks`, title);
+    }
+
+    static updateTask(todoListID: string, taskID: string, data: UpdateTaskModelType) {
+        return instance.put<
+            ResponseType<TaskResponseType>,
+            AxiosResponse<ResponseType<TaskResponseType>>,
+            UpdateTaskModelType
+        >(`todo-lists/${todoListID}/${taskID}`, data);
+    }
+
+    static deleteTask(todoListID: string, taskID: string) {
+        return instance.delete<ResponseType>(`todo-lists/${todoListID}/tasks/${taskID}`);
+    }
+
+    static reorderTask(todoListID: string, taskID: string, data: { putAfterItemId: string }) {
+        return instance.put<ResponseType>(`todo-lists/${todoListID}/tasks/${taskID}`);
+    }
+}
+
+export type TodoListServerType = {
+    id: string;
+    title: string;
+    addedDate: string;
+    order: number;
+};
 export type TaskType = {
     description: string;
     title: string;
@@ -32,7 +118,6 @@ export type TaskType = {
     order: number;
     addedDate: string;
 };
-
 export type UpdateTaskModelType = {
     title: string;
     description: string;
@@ -40,4 +125,40 @@ export type UpdateTaskModelType = {
     priority: TaskPriorities;
     startDate: string;
     deadline: string;
+};
+export type ResponseType<T = {}> = {
+    resultCode: number;
+    messages: string[];
+    data: T;
+};
+type AuthResponseType = {
+    id: number;
+    email: string;
+    login: string;
+};
+type LoginRequestType = {
+    email: string;
+    password: string;
+    rememberMe?: boolean;
+};
+type LoginResponseType = {
+    userId: number;
+};
+type CreateTodoListType = {
+    item: {
+        id: string;
+        title: string;
+        addedDate: string;
+        order: number;
+    };
+};
+type GetTasksResponseType = {
+    items: TaskType[];
+    totalCount: number;
+    error: null | string;
+};
+type TaskResponseType = {
+    data: {
+        item: TaskType;
+    };
 };
